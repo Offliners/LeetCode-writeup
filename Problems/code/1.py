f37e945d4088fda223f56bdc5d44fe391b43b14e
Offliner <42777325+Offliners@users.noreply.github.com>
2020-02-16 22:48:25 +0800

    Update 1.py

--- Problems/code/1.py
+++ Problems/code/1.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+        numSet = {}
         for i, num in enumerate(nums):
-            if target - num in num_to_index:
-                return [num_to_index[target - num], i]
-            num_to_index[num] = i
+            if target - num in numSet:
+                return [numSet[target - num], i]
+            numSet[num] = i
+            
         return []
