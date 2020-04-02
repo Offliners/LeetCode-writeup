9b4335221d0d7b9f292f2b2d4df14e23d49a3ed4
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-02 23:46:52 +0800

    Update singleNumber.c

--- Activity/Week1/C/singleNumber.c
+++ Activity/Week1/C/singleNumber.c
@@ -1,36 +1,7 @@
 int singleNumber(int* nums, int numsSize){
-    int n;
-    
-    numSort(nums, numsSize);
-    n = nums[0];
+    int n = nums[0];
     
     for(int i = 1; i < numsSize; i++)
         n ^= nums[i];
     return n;
-}
-
-void numSort(int list[],int n)
-{
-	int min;
-
-	for(int i = 0;i < n;i++)
-	{
-		min = i;
-
-		for(int j = i + 1;j < n;j++)
-		{
-			if(list[j] < list[min])
-				min = j;
-		}
-		Swap(&list[i],&list[min]);
-	}
-}
-
-void Swap(int *a,int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
 }
