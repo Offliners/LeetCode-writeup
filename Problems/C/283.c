3706d1e879dece013abcb11671cb45ff0e885d20
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-05 14:00:43 +0800

    Create 283.c

---
+++ Problems/C/283.c
@@ -0,0 +1,15 @@
+void moveZeroes(int* nums, int numsSize){
+    int count = 0;
+    
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] != 0)
+        {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    
+    for(;count < numsSize; count++)
+        nums[count] = 0;   
+}
