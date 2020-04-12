c94a7ab79306a71b1ea4365637ea86990e7c6925
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-13 01:42:05 +0800

    Update Diameter-of-Binary-Tree.py

--- Activity/Week2/Python/Diameter-of-Binary-Tree.py
+++ Activity/Week2/Python/Diameter-of-Binary-Tree.py
@@ -1,1 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        
+        self.result = 0
+        
+        def maxDepth(node):
+            if not node:
+                return -1
+            
+            maxLeft = maxDepth(node.left)
+            maxRight = maxDepth(node.right)
+            self.result = max(self.result, maxLeft + maxRight + 2)
+            
+            return max(maxLeft + 1, maxRight + 1)
+        
+        maxDepth(root)
+        
+        return self.result
