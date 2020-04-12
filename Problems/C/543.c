d2c8e06bae933d41b527aefd4f9ab99fd600f573
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-13 01:43:25 +0800

    Update 543.c

--- Problems/C/543.c
+++ Problems/C/543.c
@@ -1,1 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode *root)
+{
+    if(root == NULL) 
+        return 0;
+    
+    int maxLeft = maxDepth(root->left);
+    int maxRight = maxDepth(root->right);
+    
+    if(maxLeft > maxRight)
+        return maxLeft + 1;
+    else
+        return maxRight + 1;
+}
 
+int diameterOfBinaryTree(struct TreeNode* root){
+    if(root == NULL)
+        return 0;
+    
+    int middle = maxDepth(root->left) + maxDepth(root->right);
+    int left = diameterOfBinaryTree(root->left);
+    int right = diameterOfBinaryTree(root->right);
+    int max = middle;
+    
+    if(left > max)
+        max = left;
+    if(right > max)
+        max = right;
+    
+    return max;
+}
