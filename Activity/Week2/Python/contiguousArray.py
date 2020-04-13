30973543e411233a27ebcde4ff88575889000c8e
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-14 01:31:26 +0800

    Update contiguousArray.py

--- Activity/Week2/Python/contiguousArray.py
+++ Activity/Week2/Python/contiguousArray.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        maxLength = 0
+        Diff = 0
+        Diffs = {0: -1}
+        
+        for i, num in enumerate(nums):
+            if num == 0:
+                Diff += 1
+            else:
+                Diff -= 1
+                
+            if Diff in Diffs:
+                maxLength = max(maxLength, i - Diffs[Diff])
+            else:
+                Diffs[Diff] = i
+                
+        return maxLength
