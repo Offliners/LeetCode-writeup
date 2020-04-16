ec482329189fd2194f449ecc808ae23f0ecf1f8f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-16 19:11:22 +0800

    Update perform-string-shifts.py

--- Activity/Week2/Python/perform-string-shifts.py
+++ Activity/Week2/Python/perform-string-shifts.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        strLen = len(s)
+        total = 0
+        
+        for i in range(len(shift)):
+            direction = shift[i][0]
+            amount = shift[i][1]
+            
+            amount %= strLen;
+            if direction == 1:
+                amount = strLen - amount
+                
+            total += amount
+            
+        total %= strLen
+        s = list(s)
+        for _ in range(total):
+            temp = s[0]
+            for i in range(strLen-1):
+                s[i] = s[i + 1]
+                
+            s[strLen - 1] = temp
+        
+        return "".join(s)
