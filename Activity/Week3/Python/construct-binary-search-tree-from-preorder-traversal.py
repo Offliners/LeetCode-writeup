47ecc08a68eebdb0bbe902e994bc2e9a55623534
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-20 23:28:09 +0800

    Update construct-binary-search-tree-from-preorder-traversal.py

--- Activity/Week3/Python/construct-binary-search-tree-from-preorder-traversal.py
+++ Activity/Week3/Python/construct-binary-search-tree-from-preorder-traversal.py
@@ -1,1 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        self.topIndex = 0;
+        
+        def buildTree(max = float('inf')):
+            if self.topIndex >= len(preorder) or preorder[self.topIndex] > max:
+                return None
+            
+            root = TreeNode(preorder[self.topIndex])
+            self.topIndex += 1
+            root.left = buildTree(root.val)
+            root.right = buildTree(max)
+            
+            return root
+        
+        return buildTree()
