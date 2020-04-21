7137d1704610fe21318712098f71fb4ec56955b8
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-21 21:28:20 +0800

    Update leftmost-column-with-at-least-a-one.py

--- Activity/Week3/Python/leftmost-column-with-at-least-a-one.py
+++ Activity/Week3/Python/leftmost-column-with-at-least-a-one.py
@@ -1,1 +1,24 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
 
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        row, col = binaryMatrix.dimensions()
+        leftMost = col
+        
+        for i in range(row):
+            while binaryMatrix.get(i, leftMost - 1) == 1:
+                leftMost -= 1
+                
+                if leftMost == 0:
+                    return 0
+                
+        if leftMost == col:
+            return -1
+        
+        return leftMost
