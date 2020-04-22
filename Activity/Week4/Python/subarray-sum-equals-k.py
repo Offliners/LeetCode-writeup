c9dfcb583b061f4e40203bf5031534eb8517e0ce
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-23 00:43:40 +0800

    Update subarray-sum-equals-k.py

--- Activity/Week4/Python/subarray-sum-equals-k.py
+++ Activity/Week4/Python/subarray-sum-equals-k.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        total = 0
+        hashTable = defaultdict(int)
+        sum = 0
+        
+        for num in nums:
+            sum += num
+            
+            if sum == k:
+                total += 1
+            
+            if sum - k in hashTable:
+                total += hashTable[sum - k]
+                
+            hashTable[sum] += 1
+            
+        return total
