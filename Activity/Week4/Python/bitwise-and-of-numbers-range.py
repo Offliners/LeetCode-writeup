b7ff72529ea4f3e93df79dd3a010a0994dd48cfe
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-23 20:10:17 +0800

    Update bitwise-and-of-numbers-range.py

--- Activity/Week4/Python/bitwise-and-of-numbers-range.py
+++ Activity/Week4/Python/bitwise-and-of-numbers-range.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        if m == 0:
+            return 0
+        
+        result = 0
+        bit = int(log(n, 2))
+        
+        while bit >= 0 and ((m &  (1 << bit)) == (n & (1 << bit))):
+            if (m & (1 << bit)):
+                result += 2**bit
+                
+            bit -= 1
+        
+        return result
