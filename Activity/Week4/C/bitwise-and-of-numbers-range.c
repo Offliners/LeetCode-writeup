ef5a5033b1c2c8ff427591da712c1917da0889a7
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-23 20:30:54 +0800

    Update bitwise-and-of-numbers-range.c

--- Activity/Week4/C/bitwise-and-of-numbers-range.c
+++ Activity/Week4/C/bitwise-and-of-numbers-range.c
@@ -1,1 +1,12 @@
-
+int rangeBitwiseAnd(int m, int n){
+    int count = 0;
+    
+    while(m != n)
+    {
+        m = m >> 1;
+        n = n >> 1;
+        count++;
+    }
+    
+    return m << count;
+}
