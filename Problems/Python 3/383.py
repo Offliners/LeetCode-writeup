09bbc8d156ab2489ef42be2cfb5b24588066490c
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-05 22:44:52 +0800

    Update 383.py

--- Problems/Python 3/383.py
+++ Problems/Python 3/383.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazineCount = Counter(magazine)
+        ransomNoteCount = Counter(ransomNote)
+        
+        for c in ransomNoteCount:
+            if c not in ransomNoteCount or magazineCount[c] < ransomNoteCount[c]:
+                return False
+            
+        return True
