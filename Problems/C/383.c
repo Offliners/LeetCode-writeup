5710c86b19217ff97a74a53912ffe159f8fc25fa
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-05 22:54:07 +0800

    Update 383.c

--- Problems/C/383.c
+++ Problems/C/383.c
@@ -1,1 +1,28 @@
+int cmp(const void *a, const void *b)
+{
+    return *(const char*)a - *(const char*)b;
+}
 
+bool canConstruct(char * ransomNote, char * magazine){
+    int ransomNoteSize = strlen(ransomNote);
+    int magazineSize = strlen(magazine);
+    int i = 0, j = 0;
+    
+    qsort(ransomNote, ransomNoteSize, sizeof(char), cmp);
+    qsort(magazine, magazineSize, sizeof(char), cmp);
+    
+    while((i != ransomNoteSize)&&(j != magazineSize))
+    {
+        if(ransomNote[i] == magazine[j])
+        {
+            i++;
+            j++;
+        }
+        else if(ransomNote[i] < magazine[j])
+            return false;
+        else
+            j++;
+    }
+    
+    return i == ransomNoteSize;
+}
