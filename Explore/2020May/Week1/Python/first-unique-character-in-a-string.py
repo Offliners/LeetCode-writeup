c7310f2be6c988601bd5666df4ca26ba9081ce55
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-09 13:55:56 +0800

    Update first-unique-character-in-a-string.py

--- Explore/2020May/Week1/Python/first-unique-character-in-a-string.py
+++ Explore/2020May/Week1/Python/first-unique-character-in-a-string.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = [0] * 26
+        
+        for i in s:
+            count[ord(i) - ord('a')] += 1
+            
+        for i, c in enumerate(s):
+            if count[ord(c) - ord('a')] == 1:
+                return i;
+            
+        return -1
