9e8bd261c5c7fea85eb0570cb494491f3beae3a5
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-10 20:49:24 +0800

    Update 1232.py

--- Problems/Python 3/1232.py
+++ Problems/Python 3/1232.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if len(coordinates) < 3:
+            return True
+        
+        xDiff = coordinates[1][0] - coordinates[0][0]
+        yDiff = coordinates[1][1] - coordinates[0][1]
+        
+        for x, y in coordinates[2:]:
+            dx = x - coordinates[0][0]
+            dy = y - coordinates[0][1]
+            
+            if xDiff * dy != yDiff * dx:
+                return False
+        
+        return True
