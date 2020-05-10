872d6ed5a433feb17d5c15b45fa39661289e60fd
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-10 21:19:57 +0800

    Update 997.py

--- Problems/Python 3/997.py
+++ Problems/Python 3/997.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        trustNum = [0] * (N + 1)
+        
+        for trustee, trusted in trust:
+            trustNum[trusted] += 1
+            trustNum[trustee] -= 1
+            
+        for people in range(1, N + 1):
+            if trustNum[people] == N - 1:
+                return people
+        
+        return -1
