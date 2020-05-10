16e830766daeaeab13bcba9e2f27f03a8e881e69
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-10 21:30:55 +0800

    Update 997.c

--- Problems/C/997.c
+++ Problems/C/997.c
@@ -1,1 +1,18 @@
-
+int findJudge(int N, int** trust, int trustSize, int* trustColSize){
+    int *trustNum = malloc(sizeof(int) * (N + 1));
+    
+    for(int i = 0; i < N + 1; i++)
+        trustNum[i] = 0;
+    
+    for(int i = 0; i < trustSize; i++)
+    {
+        trustNum[trust[i][0]] -= 1;
+        trustNum[trust[i][1]] += 1;
+    }
+    
+    for(int i = 1; i < N + 1; i++)
+        if(trustNum[i] == N - 1)
+            return i;
+    
+    return -1;
+}
