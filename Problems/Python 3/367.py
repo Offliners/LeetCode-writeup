af94669291a21408924c0c4c966d78b3942bafac
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-10 21:03:18 +0800

    Update 367.py

--- Problems/Python 3/367.py
+++ Problems/Python 3/367.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 1, num
+        
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            
+            if square == num:
+                return True
+            
+            if square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
