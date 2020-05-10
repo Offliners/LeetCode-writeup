37235628c2bef240da25cb449f4038443e55001c
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-10 21:08:27 +0800

    Update valid-perfect-square.c

--- Explore/2020May/Week2/C/valid-perfect-square.c
+++ Explore/2020May/Week2/C/valid-perfect-square.c
@@ -1,1 +1,20 @@
-
+bool isPerfectSquare(int num){
+    int left = 0;
+    int right = num;
+    
+    while(left <= right)
+    {
+        long long mid = (left + right) / 2;
+        long long square = mid * mid;
+        
+        if(square == num)
+            return true;
+        
+        if(square < num)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    
+    return false;
+}
