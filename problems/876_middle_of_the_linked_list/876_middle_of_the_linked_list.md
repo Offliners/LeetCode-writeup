88c82efd6282b52b4fc948e06574d233c45151d7
Offliners <eric21904@gmail.com>
2024-01-01 01:27:00 +0800

    update 876. Middle of the Linked List

---
+++ problems/876_middle_of_the_linked_list/876_middle_of_the_linked_list.md
@@ -0,0 +1,12 @@
+# 876. Middle of the Linked List
+Link: [link](https://leetcode.com/problems/middle-of-the-linked-list/)
+
+### Solution 1 - Two Pointers
+* Time complexity: $O(1)$
+* Space complexity: $O(1)$
+
+### Code
+||Sol 1||
+|-|-|
+|C|[link](./sol_1/main.c)|
+|C++|[link](./sol_1/main.cpp)|
---
+++ problems/876_middle_of_the_linked_list/sol_1/main.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    if(!head || !head->next)
+        return head;
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
